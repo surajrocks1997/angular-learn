141d420381ffe9795c61a93de0e32646163fc710
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-11 16:28:28 +0530

    two way data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-
-export class ServerComponent { 
+export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,24 +8,27 @@ import { Component, OnInit } from '@angular/core';
   // <app-server></app-server>
   // <app-server></app-server>`,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
-  serverCreationStatus = "no server was created";
+  serverCreationStatus = 'no server was created';
+  serverName = 'TestServer';
 
-  constructor() { 
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = "server was created";
+    this.serverCreationStatus = 'server was created';
   }
 
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
